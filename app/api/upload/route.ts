1b7d272f6864057c5d77e9553e4db49b141c813b
SHAZ1786 <zeven074@gmail.com>
2026-06-03 17:32:34 +0530

    .

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -4,11 +4,7 @@ import cloudinary from "@/lib/cloudinary";
 export async function POST(request: Request) {
   try {
 
-    console.log({
-      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-      api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDINARY_API_SECRET ? "EXISTS" : "MISSING",
-    });
+    
     const formData = await request.formData();
 
     const file = formData.get("file") as File;
@@ -28,6 +24,11 @@ export async function POST(request: Request) {
     const buffer = Buffer.from(bytes);
 
     const base64 = `data:${file.type};base64,${buffer.toString("base64")}`;
+    console.log({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET ? "EXISTS" : "MISSING",
+    });
 
     const result = await cloudinary.uploader.upload(base64, {
       folder: "resolute",
