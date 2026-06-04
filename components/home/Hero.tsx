3abcd029cf17c7b313a950044176602e816198b7
SHAZ1786 <zeven074@gmail.com>
2026-06-04 15:00:56 +0530

    Update Hero.tsx

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -40,7 +40,7 @@ export default function Hero({
     return null;
 
   return (
-    <section className="relative w-full aspect-[16/7] overflow-hidden">
+    <section className="relative w-full aspect-[16/7] overflow-hidden mt-10">
       <AnimatePresence mode="wait">
         <motion.div
           key={index}
